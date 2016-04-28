740151ca5feabf0fbfb7ee424d4f897957652909
Adam Oswalt <adam.oswalt@gmail.com>
2016-04-28 14:43:52 -0500

    Inserting songs into DOM from objects

--- songs.js
+++ songs.js
@@ -1,18 +1,60 @@
-var songs = [];
+var songList = [
+  {
+    title: "Legs",
+    artist: "ZZ Top",
+    album: "Eliminator",
+    genre: "Rock"
+  },
+  {
+    title: "The Logical Song",
+    artist: "Supertramp",
+    album: "Breakfast in America",
+    genre: "Pop"
+  },
+  {
+    title: "Another Brick in the Wall",
+    artist: "Pink Floyd",
+    album: "The Wall",
+    genre: "Rock"
+  },
+  {
+    title: "Welcome to the Jungle",
+    artist: "Guns & Roses",
+    album: "Appetite for Destruction",
+    genre: "Rock"
+  },
+  {
+    title: "Ironic",
+    artist: "Alanis Morisette",
+    album: "Jagged Little Pill",
+    genre: "Pop"
+  },
+  {
+    title: "The Air That I Breathe",
+    artist: "All That Remains",
+    album: "The Fall of Ideals",
+    genre: "Rock"
+  },
+  {
+    title: "Holy Diver",
+    artist: "Killswitch Engage",
+    album: "As Daylight Dies",
+    genre: "Rock"
+  }
+];
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+(function() {
+  var songArea = document.getElementById("songs");
 
-songs.unshift("The Air That I Breathe by All That Remains on the album The Fall of Ideals");
-songs.push("Holy Diver by Killswitch Engage on the album As Daylight Dies");
+  function insertSong(song) {
+    songArea.innerHTML += `
+      <section class="song-info">
+        <h1>${song.title}Song Name</h1>
+        <span class="artist-name">${song.artist}</span>
+        <span class="album-name">${song.album}</span>
+        <span class="genre">${song.genre}</span>
+      </section>`;
+  }
 
-var body = document.getElementsByTagName("body")[0];
-for (var i = 0; i < songs.length; ++i) {
-  songs[i] = songs[i].replace(/[^\w\s>]/g, "");
-  songs[i] = songs[i].replace(">", "-");
-
-  body.innerHTML += `<p>${songs[i]}</p>`;
-}
+  songList.forEach((s) => { insertSong(s); });
+}());
